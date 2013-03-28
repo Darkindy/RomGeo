3ccaac08cb1bcfd479359e6b9c0f92689704d324
alex67x <alex67x@gmail.com>
2013-03-29 01:03:21 +0200

    testing repo

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace RomGeo
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. testing repo
         /// </summary>
         [STAThread]
         static void Main()
